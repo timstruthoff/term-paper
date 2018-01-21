5b55cdb579eb7e3f2a55ed128ae6d769784c7419
Tim Struthoff <tim.struthoff@gmail.com>
2018-01-21 09:29:41 +0100

    server_001

--- app/server/index.js
+++ app/server/index.js
@@ -1,1 +1,17 @@
-console.log("test2");
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render(__dirname + '/index.handlebars', {
+        test_var:"test123",
+        hello_world:"hello_world"
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
