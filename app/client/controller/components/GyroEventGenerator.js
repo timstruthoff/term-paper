4a20985d3bc909c6d19174226ce5f356cf8f5e91
Tim Struthoff <tim.struthoff@gmail.com>
2018-02-09 16:38:05 +0100

    added gyro normalize

--- app/client/controller/components/GyroEventGenerator.js
+++ app/client/controller/components/GyroEventGenerator.js
@@ -3,14 +3,18 @@ export default class {
     constructor(args) {
         window.addEventListener('deviceorientation', (e) => {
             this.onChange({
-                beta: e.beta
+                beta: this.normalize(e.beta)
             });
         }, false);
         window.ondeviceorientation = (e) => {
             this.onChange({
-                beta: e.beta
+                beta: this.normalize(e.beta)
             });
         }
+    }
+
+    normalize(value) {
+        return (value / 360) + 0.5
     }
 
     onChange (data) {
