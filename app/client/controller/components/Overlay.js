c100e4bdc64c5ce6a39a66aec80b06bc708f1e96
Tim Struthoff <tim.struthoff@gmail.com>
2018-02-10 16:53:47 +0100

    added overlay class

---
+++ app/client/controller/components/Overlay.js
@@ -0,0 +1,23 @@
+export default class {
+
+    constructor(message) {
+        this.containerEl = document.createElement('div');
+
+        this.containerEl.style.position = 'fixed';
+        this.containerEl.style.width = '100%';
+        this.containerEl.style.height = '100%';
+        this.containerEl.style.backgroundColor = '#fff';
+        this.containerEl.style.position = 'fixed';
+
+
+        this.pEl = document.createElement('p');
+        this.pEl.innerHTML = message;
+
+        this.containerEl.appendChild(this.pEl);
+        document.body.appendChild(this.containerEl);
+    }
+
+    destroy() {
+
+    }
+}
