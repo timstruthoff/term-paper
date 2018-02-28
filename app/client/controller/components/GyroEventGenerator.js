9ecf97c7a0fd4ec435705e1155bf948aaebe1f50
Tim Struthoff <tim.struthoff@gmail.com>
2018-02-28 17:44:00 +0100

    added sensitivity

--- app/client/controller/components/GyroEventGenerator.js
+++ app/client/controller/components/GyroEventGenerator.js
@@ -1,3 +1,5 @@
+import CONFIG from './../../config'
+
 export default class {
 
     constructor(args) {
@@ -14,7 +16,12 @@ export default class {
     }
 
     normalize(value) {
-        return (value / 360) + 0.5
+        let normalized = (value / 360) + 0.5;
+        
+        let sensitivity = 1 / CONFIG.CONTROLLER_SENSITIVITY;
+        let lowerBorder = (1 - sensitivity) / 2;
+        let upperBorder = 1 - ((1 - sensitivity) / 2);
+        return (Math.min(upperBorder, Math.max(lowerBorder, normalized)) - lowerBorder) * (1 / sensitivity); // limits number to 0 - 1
     }
 
     onChange (data) {
